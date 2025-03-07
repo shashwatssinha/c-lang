d717652148118c7dad17798ac8061eeb49628724
shashwatssinha <shashwatumred@gmail.com>
2025-03-07 22:29:55 +0530

    Add files via upload
    basic use of pointer

---
+++ pointer.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    int i=5;
+    int* j=&i;
+    printf("the address of i is %p\n",&i);
+    printf("the address of i is %p\n",j);
+    printf ("the value at adress j is %d\n",*(&i));
+
+    return 0;
+}
